94070749eb956e4d11fc29fce5d3c0815b493b6c
Ulises Jabriel Reyes Cuevas <2022-0678@unad.edu.do>
2025-06-11 15:58:50 -0400

    Add files via upload

---
+++ Descuento por edad 05/Descuento por edad 05/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Descuento_por_edad_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite su edad: ");
+            int edad = int.Parse(Console.ReadLine());
+
+            if (edad > 60)
+            {
+                Console.WriteLine("El descuento del 50% si aplica");
+            }
+            else 
+            {
+            Console.WriteLine("El descuento no aplica");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
