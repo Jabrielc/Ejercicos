a272833facf33a624662d62b96b329e0e38ec708
Ulises Jabriel Reyes Cuevas <2022-0678@unad.edu.do>
2025-06-11 15:48:46 -0400

    Add files via upload

---
+++ Mayor de edad 03/Mayor de edad 03/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mayor_de_edad_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite su edad: ");
+            int edad = int.Parse(Console.ReadLine());
+
+            if (edad >= 18)
+            {
+                Console.WriteLine("Es mayor de edad: ");
+            }
+            else 
+            { 
+            Console.WriteLine("Es menor de edad: ");
+            }
+        Console.Read();
+        }
+    }
+}
