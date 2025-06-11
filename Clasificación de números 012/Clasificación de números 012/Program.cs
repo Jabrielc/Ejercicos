08e21ad5cb8fd829cfaf88dcbe607d09443fdb6c
Ulises Jabriel Reyes Cuevas <2022-0678@unad.edu.do>
2025-06-11 16:01:53 -0400

    Add files via upload

---
+++ Clasificación de números 012/Clasificación de números 012/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clasificación_de_números_012
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite el primer número: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite el segundo número: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite el tercer número: ");
+            int num3 = int.Parse(Console.ReadLine());
+
+            if (num1 == 0 || num2 == 0 || num3 == 0)
+            {
+                Console.WriteLine("Hay ceros");
+            }
+            else if (num1 > 0 && num2 > 0 && num3 > 0)
+            {
+                Console.WriteLine("Todos son positivos");
+            }
+            else if (num1 < 0 && num2 < 0 && num3 < 0)
+            {
+                Console.WriteLine("Todos son negativos");
+            }
+            else
+            {
+                Console.WriteLine("Son mixtos");
+            }
+
+        }
+    }
+}
---
+++ Comparación de tres longitudes 015/Comparación de tres longitudes 015/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comparación_de_tres_longitudes_015
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite el lado 1: ");
+            int lado1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite el lado 2: ");
+            int lado2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite el lado 3: ");
+            int lado3 = int.Parse(Console.ReadLine());
+
+            if (lado1 + lado2 > lado3 && lado1 + lado3 > lado2 && lado2 + lado3 > lado1)
+            {
+                Console.WriteLine("Pueden Formar Un Triangulo");
+            }
+            else
+            { 
+                Console.WriteLine("No Pueden Formar un Triangulo");
+            }
+             Console.ReadLine();
+        }
+    }
+}
